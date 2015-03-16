a11097a6f8dfa76bd7414ad2fe34d8af43743cb8
Ahsan Bagwan <ahsanbagwan@gmail.com>
2015-03-16 16:29:09 +0530

    Added service code and controller

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -7,11 +7,48 @@
  * # AboutCtrl
  * Controller of the apiconsumeApp
  */
-angular.module('apiconsumeApp')
-  .controller('AboutCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+
+angular.module('apiconsumeApp').controller('AboutCtrl',
+			['$rootScope', '$scope', 'WebsiteService',
+ function($rootScope, $scope, WebsiteService) {
+   /* var setup = function( pageNumber ) {
+        WebsiteService
+            .load()
+            .then( function( websiteResource ) {
+                return websiteResource.$get('blogs', {'page': pageNumber, 'size':10, 'sort':null});
+            })
+            .then( function( resource )
+            {
+                $scope.page = resource.page;
+                $scope.page.currentPage = $scope.page.number + 1;
+                return resource.$get('blogPostList');
+            })
+            .then( function( blogPostList )
+            {
+                $scope.blogs = blogPostList;
+                blogPostList.forEach( function( blog ) {
+                    // load author profile
+                    blog.$get('author').then(function(author) {
+                        blog.author = author;
+                    });
+
+                });
+            });
+
+    };
+
+    setup(0);
+
+    $scope.selectBlogPage = function(pageNumber) {
+        setup(pageNumber-1); //Spring HATEOAS page starts with 0
+    };
+*/
+
+	 WebsiteService
+            .load()
+            .then( function( websiteResource ) {
+            	console.log(websiteResource );
+            });
+
+}
+]);  
---
+++ app/scripts/services/websiteservice.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name apiconsumeApp.WebsiteService
+ * @description
+ * # WebsiteService
+ * Factory in the apiconsumeApp.
+ */
+angular.module('apiconsumeApp')
+  .factory('WebsiteService', ['halClient',function () {
+    
+    // Public API here
+    return {
+      'load': function () {
+        return halClient.$get('https://www.reddit.com/.json');
+      }
+    };
+  }]);
